16b24c91da88bff4200071af86a163dacf5febc0
Jeff Cagle <jc.cagle@pm.me>
2021-12-06 21:52:59 -0600

    Update about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,45 +1,16 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { graphql } from 'gatsby';
-import {
-  GatsbyImage,
-  GatsbyImageProps,
-  getImage,
-  StaticImage,
-} from 'gatsby-plugin-image';
+import { StaticImage } from 'gatsby-plugin-image';
 import ListTools from '../components/lists/ListTools';
 import { Box, Section } from '../components/shared/Ui';
 import { Row, Column } from '../components/shared/Columns';
-import SocialLinks from '../components/shared/SocialLinks';
-import HeroBar from '../components/shared/HeroBar';
+import ListBooks from '../components/lists/ListBooks';
 
-interface AboutProps {
-  data: {
-    allBooksJson: {
-      nodes: BookProps[];
-    };
-  };
-}
-
-interface BookProps {
-  id: string;
-  title: string;
-  author: string;
-  amazonUrl: string;
-  image: GatsbyImageProps['image'];
-}
-
-const About = ({ data }: AboutProps) => {
-  const books = data.allBooksJson.nodes;
-
+const About = () => {
   return (
     <>
-      <HeroBar py={3}>
+      <Section>
         <h1>About</h1>
-        <p>This is the about page...</p>
-      </HeroBar>
-      <Section h2="Who Am I?">
         <Row>
           <Column width={50}>
             <p>
@@ -49,7 +20,11 @@ const About = ({ data }: AboutProps) => {
               consectetur adipisicing elit. Molestias aut, repellat ipsum facere
               voluptate dicta obcaecati deserunt nobis suscipit eaque.
             </p>
-            <SocialLinks startText="Find me on: " />
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Molestias aut, repellat ipsum facere voluptate dicta obcaecati
+              deserunt nobis suscipit eaque.
+            </p>
           </Column>
           <Column width={50}>
             <Box flex justifyRight>
@@ -64,43 +39,30 @@ const About = ({ data }: AboutProps) => {
         </Row>
       </Section>
 
-      <Section h2="Favorite Dev Tools">
+      <Section h2="Tech I Like">
         <ListTools
-          startText="I love:"
           items={[
             'JavaScript',
-            'TypeScript',
             'React',
+            'Next.js',
+            'Gatsby',
+            'TypeScript',
             'Python',
-            'Styled Components',
-            'SASS',
-            'Photoshop',
             'Figma',
+            'Linux',
+            'VSCode',
+            'GraphQL',
+            'CSS3',
+            'SASS',
           ]}
         />
       </Section>
 
-      <Section h2="Some Books I Like">
-        <Row>
-          {books.map((book: BookProps) => (
-            <Column width={20} key={book.id}>
-              <BookItem href={book.amazonUrl}>
-                {
-                  // @ts-ignore
-                  <BookImage image={getImage(book.image)} alt={book.title} />
-                }
-                <Title>{book.title}</Title>
-              </BookItem>
-            </Column>
-          ))}
-        </Row>
+      <Section h2="Books I Like">
+        <ListBooks />
       </Section>
     </>
   );
-};
-
-About.propTypes = {
-  data: PropTypes.oneOfType([PropTypes.object]).isRequired,
 };
 
 const ProfileImage = styled.div`
@@ -109,74 +71,6 @@ const ProfileImage = styled.div`
   border-radius: 125px;
   overflow: hidden;
   border: 10px solid ${props => props.theme.colors.neutral700};
-`;
-
-const BookItem = styled.a`
-  padding: 0.7rem;
-  color: ${props => props.theme.colors.neutral300};
-  background: ${props => props.theme.colors.neutral700};
-  border: 2px solid ${props => props.theme.colors.neutral700};
-  border-radius: 10px;
-  display: flex;
-  flex-direction: column;
-  justify-content: start;
-  position: relative;
-  width: 100%;
-  height: 100%;
-  margin-top: 0;
-  margin-bottom: 0;
-  margin-left: 0;
-  margin-right: 0;
-  transition: width 0.2s, height 0.2s, margin 0.2s,
-    0.2s cubic-bezier(0.37, 0, 0.65, 1);
-
-  &:hover {
-    /* color: ${props => props.theme.colors.neutral100}; */
-    color: ${props => props.theme.colors.primary};
-    border: 2px solid ${props => props.theme.colors.secondary};
-    box-shadow: 0 3px 10px rgba(0, 0, 0, 0.5);
-    width: 102%;
-    height: 102%;
-    margin-top: -2%;
-    margin-left: -1%;
-    margin-right: -1%;
-  }
-
-  .gatsby-image-wrapper {
-    margin-bottom: 1rem;
-    border-radius: 5px 5px 0 0;
-  }
-`;
-
-const BookImage = styled(GatsbyImage)``;
-
-const Title = styled.span`
-  display: block;
-  line-height: 1.8rem;
-  font-weight: bold;
-  text-align: center;
-`;
-
-export const query = graphql`
-  query getBooks {
-    allBooksJson {
-      nodes {
-        id
-        title
-        author
-        image {
-          childImageSharp {
-            gatsbyImageData(
-              width: 250
-              height: 350
-              transformOptions: { cropFocus: CENTER }
-            )
-          }
-        }
-        amazonUrl
-      }
-    }
-  }
 `;
 
 export default About;
